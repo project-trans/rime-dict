a1a9383076f42addb8770e7ec17b7a13e98b86a4
北雁 Cryolitia <Cryolitia@gmail.com>
2023-11-14 19:12:23 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# 跨儿计划 Rime 词典
+
+## 语料来源
+
+- [MtF.wiki](https://github.com/project-trans/MtF-wiki)
+- [FtM.wiki](https://github.com/project-trans/FtM-wiki)
+- [RLE.wiki](https://github.com/project-trans/RLE-wiki)
+- [女性倾向跨性别者科学](https://github.com/project-trans/transfeminine-science)
+- [中华人民共和国跨性别相关法律法规变迁](https://github.com/project-trans/legal-spec)
+
+### 停用词
+
+[现代汉语常用词表](https://gist.github.com/indiejoseph/eae09c673460aa0b56db)
+
+## Rime 词典
+
+词典中包含除常用词表外的汉语词汇及词频.
+
+可在 Release 中获取
+
+## 词云
+
+### 完整
+
+![result_full](./result_full.png)
+
+### 停用常用词
+
+![result](./result.png)
---
+++ main.py
@@ -0,0 +1,72 @@
+import os
+import collections
+
+from markdown_it import MarkdownIt
+from mdit_plain.renderer import RendererPlain
+import regex
+import wordcloud
+import matplotlib
+import pkuseg
+from pypinyin import lazy_pinyin
+
+# get all markdown files
+file_list: list[str] = []
+for filepath, dirnames, filenames in os.walk("."):
+    for filename in filenames:
+        if filename.endswith(".md"):
+            file_list.append(os.path.join(filepath, filename))
+
+# read sentences
+sentence_list: list[str] = []
+pattern = regex.compile("\\p{Han}+")
+parser: MarkdownIt = MarkdownIt(renderer_cls=RendererPlain)
+for file in file_list:
+    plain_data: str = parser.render(open(file, "r").read())
+    sentences = plain_data.split("\n")
+    for sentence in sentences:
+        if pattern.search(sentence):
+            sentence_list.append(sentence)
+
+# read common worlds
+common_word_lines = open("现代汉语常用词表.txt","r").readlines()
+common_word_set: set[str] = set()
+for common_word_line in common_word_lines:
+    common_word_set.add(pattern.findall(common_word_line)[0])
+
+# cut sentences
+seg = pkuseg.pkuseg(model_name="default_v2") # download from https://github.com/lancopku/pkuseg-python/releases/download/v0.0.25/default_v2.zip
+word_dict: dict[str, int] = collections.defaultdict(int)
+for sentence in sentence_list:
+    sentence2 = regex.sub("\\p{P}", " ", sentence).strip()
+    # word_list = jieba.cut(sentence2, cut_all=True, use_paddle=True)
+    word_list = seg.cut(sentence2)
+    for word in word_list:
+        word2 = regex.sub("\\s+", " ", word)
+        if len(word2) > 1 and word2 not in common_word_set and pattern.match(word2):
+            word_dict[word2] += 1
+
+# wordcloud
+colormap = matplotlib.colors.ListedColormap(["#5BCEFA", "#F5A9B8", "#2D2D2D", "#9B59D0", "#FFF433"])
+w = wordcloud.WordCloud(width=1920, height=1080, font_path="sarasa-ui-tc-regular.ttf", background_color="#7f7f7f", colormap=colormap)
+w.generate_from_frequencies(word_dict)
+#w.to_file("result.png")
+
+# build dict
+word_list = sorted(word_dict.items(), key=lambda kv: kv[1], reverse=True)
+open("result.txt", "w").write(str(word_list))
+rime_dict_str = """---
+name: project_trans
+version: "0.1"
+sort: by_weight
+...
+
+"""
+for word in word_list:
+    word_str = word[0]
+    word_str += "\t"
+    word_str += " ".join(lazy_pinyin(word[0]))
+    word_str += "\t"
+    word_str += str(word[1])
+    word_str += "\n"
+    rime_dict_str += word_str
+open("project_trans.dict.yaml", "w").write(rime_dict_str)
